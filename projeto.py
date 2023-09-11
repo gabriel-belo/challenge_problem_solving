1ac3240e2d59374240a388c0b43dc56255684cdc
gabriel-belo <rm551669@fiap.com.br>
2023-09-10 21:35:04 -0300

    aprimoramento
    Aprimoramento do projeto

--- projeto.py
+++ projeto.py
@@ -1,22 +1,25 @@
 continuar=""
 cont_carros=0
-media=0
+lista=[]
+def calculo_tempo(deslocamento:int, velocidade_media:int)->int:
+    tempo = deslocamento/velocidade_media
+    lista.append(tempo)
+    del tempo
+    return lista
 
-def calculo_tempo(deslocamento:int, velocidade_média:int, total:int)->int:
-    tempo = deslocamento/velocidade_média
-    total+= tempo
-    return total
+deslocamento=int(input("Qual a distância de deslocamento metros: "))
 
-deslocamento=int(input("Qual a distância de deslocamento: "))
 while "N" not in continuar:
-    velocidade_media=int(input("Velocidade média em km/h: "))
+    velocidade_kmph=int(input("Velocidade média em km/h: "))
+    velocidade_mpm = (velocidade_kmph * 1000) / 60
     cont_carros+=1
-    continuar=input("Gostaria de continuar: ")
-    a= calculo_tempo(deslocamento, velocidade_media)
-    
+    continuar=input("Gostaria de continuar[S/N]: ").upper().split()
+    a= calculo_tempo(deslocamento, velocidade_mpm)
 
-t= calculo_tempo(total=int)
-media= t / cont_carros
-
+# for elemento in lista:
+#     total+= elemento
+# media= total / cont_carros
+media = sum(lista) / len(lista)
+print(lista)
 print(f"Passaram {cont_carros} carros ")
-print(f"A média de tempo gasto pelos carros é {media}")
+print(f"A média de tempo gasto pelos carros é {media:.2f}")
